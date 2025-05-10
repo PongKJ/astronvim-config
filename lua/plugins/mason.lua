8d625250594ce7c186e8637b297a2033ee9ef2b5
PongKJ <pkjinfinity@outlook.com>
2025-05-10 22:31:44 +0800

    bump: update mason to v2.0

--- lua/plugins/mason.lua
+++
@@ -1,9 +0,0 @@
--- HACK: Bug in main brach, see https://github.com/williamboman/mason.nvim/pull/1882,
--- Currently usring the v2.x branch which fixes the issue
--- TODO: Remove this after merge to the main branch
-return {
-  {
-    "williamboman/mason.nvim",
-    branch = "v2.x",
-  },
-}
