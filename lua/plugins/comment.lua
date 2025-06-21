5dce089f1e59df01feb7a226c8418a97c0bfc00f
[pkjinfinity] <[pkjinfinity@outlook.com]>
2025-06-21 21:50:21 +0800

    windows: fix comment keymap

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,3 +1,4 @@
+local utils = require "utils"
 ---@type LazySpec
 return {
   {
@@ -5,8 +6,17 @@ return {
     ---@param opts AstroCoreOpts
     opts = function(_, opts)
       local maps = require("astrocore").empty_map_table()
-      maps.n["<C-/>"] = opts.mappings.n["<Leader>/"]
-      maps.x["<C-/>"] = opts.mappings.x["<Leader>/"]
+      -- We need to hanle ctrl-prefix keymap differently on Windows
+      if utils.get_os_name() == "windows" then
+        -- Actually, on Windows, <C-_> is is what neovim received
+        -- when you press <C-/> on the keyboard.
+        -- see: https://www.reddit.com/r/neovim/comments/ro2hm4/ctrl_keybinding_not_working/?tl=zh-hans
+        maps.n["<C-_>"] = opts.mappings.n["<Leader>/"]
+        maps.x["<C-_>"] = opts.mappings.x["<Leader>/"]
+      else
+        maps.n["<C-/>"] = opts.mappings.n["<Leader>/"]
+        maps.x["<C-/>"] = opts.mappings.x["<Leader>/"]
+      end
       maps.n["<Leader>/"] = false
       maps.x["<Leader>/"] = false
 
