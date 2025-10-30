17d007445a861673b747601260625cf4072d86a1
pkj <pkjinfinity@outlook.com>
2025-10-30 22:28:48 +0800

    refactor: simplify Ctrl-prefix key mapping logic on Windows

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -7,16 +7,16 @@ return {
     opts = function(_, opts)
       local maps = require("astrocore").empty_map_table()
       -- We need to hanle ctrl-prefix keymap differently on Windows
-      if utils.get_os_name() == "windows" then
-        -- Actually, on Windows, <C-_> is is what neovim received
-        -- when you press <C-/> on the keyboard.
-        -- see: https://www.reddit.com/r/neovim/comments/ro2hm4/ctrl_keybinding_not_working/?tl=zh-hans
-        maps.n["<C-_>"] = opts.mappings.n["<Leader>/"]
-        maps.x["<C-_>"] = opts.mappings.x["<Leader>/"]
-      else
-        maps.n["<C-/>"] = opts.mappings.n["<Leader>/"]
-        maps.x["<C-/>"] = opts.mappings.x["<Leader>/"]
-      end
+      -- Actually, on Windows, <C-_> is is what neovim received when you press <C-/> on the keyboard.
+      -- see: https://www.reddit.com/r/neovim/comments/ro2hm4/ctrl_keybinding_not_working/?tl=zh-hans
+      maps.n = {
+        ["<C-/>"] = opts.mappings.n["<Leader>/"],
+        ["<C-_>"] = opts.mappings.n["<Leader>/"],
+      }
+      maps.x = {
+        ["<C-/>"] = opts.mappings.x["<Leader>/"],
+        ["<C-_>"] = opts.mappings.x["<Leader>/"],
+      }
       maps.n["<Leader>/"] = false
       maps.x["<Leader>/"] = false
 
